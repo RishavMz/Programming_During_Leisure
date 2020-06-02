f933b2dabcfbc4e439b066de076e81381686e880
Rishav_mz <58086511+RishavMz@users.noreply.github.com>
2020-06-03 00:25:36 +0530

    Update tictactoe.py

--- tictactoe.py
+++ tictactoe.py
@@ -1,9 +1,8 @@
 # Author : RishavMz
 # A simple tic tac toe game
-# Three game options options :
+# Two options 
 #       1. Human vs Computer 
 #       2. Human vs Human
-#       3. Human vs Intelligent Computer
 
 import random
 player = 0
@@ -25,9 +24,9 @@ def check(li):
         return li[6]    
     return 0    
 
-# This function tactically chooses the best possible move for the player
+# This function tactically chooses the best possible move for the computer
 def checkWin(li, inp):
-    # Searching for a move to win 
+    # Searching for a move (along columns)
     for i1 in [0,1,2]:
         if(li[i1] == inp and li[i1+3] == inp and li[i1+6] not in['0','X']): 
             print(i1 + 6,"hiihh")
@@ -36,7 +35,7 @@ def checkWin(li, inp):
             return i1
         if(li[i1+6] == inp and li[i1] == inp and li[i1+3] not in['0','X']) :
             return i1+3
-    # Searching for a move to counter opponent win        
+    # Searching for a move (along rows)      
     for i1 in [0,3,6]:
         if(li[i1] == inp and li[i1+1] == inp and li[i1+2] not in['0','X']):
             return i1 +2
@@ -44,99 +43,22 @@ def checkWin(li, inp):
             return i1
         if(li[i1+2] == inp and li[i1] == inp and li[i1+1] not in['0','X']) : 
             return i1 + 1
+    # Searching for a move (different corner cases)              
+    if(li[0] == inp and li[4] == inp and li[8] not in['0','X']):
+            return 8
+    if(li[4] == inp and li[8] == inp and li[0] not in['0','X']):
+            return 0
+    if(li[8] == inp and li[0] == inp and li[4] not in['0','X']) : 
+            return 4
+    if(li[2] == inp and li[4] == inp and li[6] not in['0','X']):
+            return 8
+    if(li[4] == inp and li[6] == inp and li[2] not in['0','X']):
+            return 0
+    if(li[6] == inp and li[2] == inp and li[4] not in['0','X']) : 
+            return 4                
     return -1          
 
-# Handles the code for Human vs computer such that theoritically the computer always wins
-def compSuperAI():
-    global player,computer
-    li = [1,2,3,4,5,6,7,8,9]
-    se = list()
-    while(True):
-        k = 0
-        print("\n-------------------")
-        for i in range(3):
-            print('|' , end="  ")
-            for j in range(3):
-                print (li[k] , end = "  |  ")
-                k+=1
-            print("\n-------------------")
-            if(k==8):
-                break
-        kk=0   
-        # Check if tie 
-        for a in range(9):
-            if(li[a] == "0" or li[a] == "X"):
-                kk+=1
-        if(kk == 9):
-            print("Its a tie")
-            break            
-        print('Select a box')
-        while(True):
-            # Player input
-            box = int(input())    
-            if(box not in se):
-                li[box-1] = '0'
-                se.append(box)
-                break
-            else:
-                print("Select a valid box")
-
-        kk=0    
-        for a in range(9):
-            if(li[a] == "0" or li[a] == "X"):
-                kk+=1
-        if(kk == 9):
-            print("Its a tie")
-            break             
-            # Computer input
-        ch = checkWin(li, 'X')
-        if(ch == -1):
-            ch = checkWin(li, '0')
-            if(ch == -1):
-                while(True):
-                    ch = random.randrange(1,10)
-                    if(ch in se):
-                        continue
-                    else:
-                        break
-                print("bug2")
-            print("bug1")
-        else:
-            print("bug3")            
-        li[ch] = 'X'            
-        se.append(ch+1)
-        # Checks if game over       
-        value = check(li)    
-        if(value == 0):
-            continue
-        elif value == "0":
-            print("Player Wins!!!")
-            player += 1
-            break
-        elif value == "X":
-            print("Computer Wins!!!")  
-            computer += 1
-            break
-    k = 0
-    print("\n-------------------")
-    for i in range(3):
-        print('|' , end="  ")
-        for j in range(3):
-            print (li[k] , end = "  |  ")
-            k+=1
-        print("\n-------------------")
-        if(k==8):
-            break 
-    # Displays final score       
-    print("\nScore: \n \t Player" , player, "\n\tComputer", computer,"\n")
-    print("Press 1 to play again, 2 to quit")
-    c = int(input())
-    if(c==1):
-        # Another game
-        comp()
-    else:
-        return    
-
+# Handles the code for Human vs computer 
 def comp():
     global player,computer
     li = [1,2,3,4,5,6,7,8,9]
@@ -178,15 +100,19 @@ def comp():
         if(kk == 9):
             print("Its a tie")
             break             
-        while(True):
             # Computer input
-            l = random.randrange(1,10)
-            if(l in se):
-                continue
-            else:
-                li[l-1] = "X"
-                se.append(l)
-                break 
+        ch = checkWin(li, 'X')      # Searching for a move to win
+        if(ch == -1):
+            ch = checkWin(li, '0')  # Searching for a move to Counter Opponent
+            if(ch == -1):
+                while(True):
+                    ch = random.randrange(0,9)
+                    if(ch+1 in se):
+                        continue
+                    else:
+                        break                
+        li[ch] = 'X'            
+        se.append(ch+1)
         # Checks if game over       
         value = check(li)    
         if(value == 0):
@@ -319,7 +245,6 @@ def mano():
 print("Welcome to tic tac toe made by me.")
 print("Enter 1 for player vs computer")
 print("Enter 2 for player vs player")
-print("Enter 3 for player vs Unbeatable Computer")
 choice = int(input())
 if(choice == 1):
     comp()
@@ -327,5 +252,3 @@ if(choice == 1):
 elif(choice == 2):
     mano()
 
-elif(choice == 3):
-    compSuperAI()    
